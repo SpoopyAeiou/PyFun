ed154a96d695bb5395e1e9bc7b4a5cc447fe6819
SpoopyAeiou <71332813+SpoopyAeiou@users.noreply.github.com>
2022-04-30 21:28:03 -0600

    Co-authored-by: Evvy-XP <Evvy-XP@users.noreply.github.com>

--- Dice.py
+++ Dice.py
@@ -1,23 +1,6 @@
 from random import randint
 
-def d20():
-    x = randint (1,20)
-    return x
-def d12():
-     x = randint (1,12)
-     return x
-def d10():
-    x = randint (1,10)
-    return x
-def d8():
-    x = randint (1,8)
-    return x
-def d6():
-    x = randint (1,6)
-    return x
-def d4():
-    x = randint (1,4)
-    return x
-def custom(y):
-    x = randint (1,y)
-    return x
+def roll_die(die):
+    n,d = die.split('d')
+    dice = randint(int(n),int(d) * int(n))
+    return(dice)
