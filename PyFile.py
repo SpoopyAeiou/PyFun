c7d9c3c874545c2f61d09a571a9e9de1fdcc97d9
SpoopyAeiou <71332813+SpoopyAeiou@users.noreply.github.com>
2022-04-30 20:04:50 -0600

    Dice implemented

--- PyFile.py
+++ PyFile.py
@@ -1,3 +1,31 @@
-def myfunction():
-    print("Hello world!")
-myfunction()
+from random import randint
+
+def d20():
+    x = randint (1,20)
+    print(x)
+d20()
+
+def d12():
+     x = randint (1,12)
+     print(x)
+d12()
+
+def d10():
+    x = randint (1,10)
+    print(x)
+d10()
+
+def d8():
+    x = randint (1,8)
+    print(x)
+d8()
+
+def d6():
+    x = randint (1,6)
+    print(x)
+d6()
+
+def d4():
+    x = randint (1,4)
+    print(x)
+d4()
