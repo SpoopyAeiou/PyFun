fd23ac2d9823289377366f6e6d2652ad073e73b9
SpoopyAeiou <71332813+SpoopyAeiou@users.noreply.github.com>
2022-04-30 21:22:10 -0600

    rivjre

--- Dice.py
+++ Dice.py
@@ -2,25 +2,22 @@ from random import randint
 
 def d20():
     x = randint (1,20)
-    return
-d20()
+    return x
 def d12():
      x = randint (1,12)
-     return
-d12()
+     return x
 def d10():
     x = randint (1,10)
-    return
-d10()
+    return x
 def d8():
     x = randint (1,8)
-    return
-d8()
+    return x
 def d6():
     x = randint (1,6)
-    return
-d6()
+    return x
 def d4():
     x = randint (1,4)
-    return
-d4()
+    return x
+def custom(y):
+    x = randint (1,y)
+    return x
